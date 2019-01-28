a82297d68d30c497b7813e8befc8c9cf2a45ed04
Eric De Wildt <eric.dewildt@publicmethod.net>
2019-01-28 18:27:41 -0500

    Add the Unit test starting comments

--- app/src/test/java/com/example/kotlinworkshop/UsersViewModelTests.kt
+++ app/src/test/java/com/example/kotlinworkshop/UsersViewModelTests.kt
@@ -2,6 +2,12 @@ package com.example.kotlinworkshop
 
 class UsersViewModelTests {
 
+    /**
+     *  1. Import the settings.jar file to get the Live Templates
+     *  2. Add the InstantTaskExecutorRule for testing LiveData
+     *  3. Use "bktest" Live Template to make the base test
+     */
 
+    // Place your test here
 
 }
